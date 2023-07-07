c62eca3822a399f6ee5d8b7215a7ee002f20c189
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-07-07 13:01:40 -0300

    Created a model and controller, with some constraints to avoid invalid input

---
+++ FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,18 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        private static List<Filme> Filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            Filmes.Add(filme);
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título é obrigatório")]
+    [StringLength(50, ErrorMessage = "O Título pode ter no máximo 50 caracteres")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero é obrigatório")]
+    [StringLength(50, ErrorMessage = "O Gênero pode ter no máximo 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "O campo de duração é obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração do filme deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
