3d7453e011d3c736779547dc2fb44b4eb70d4b11
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-08-04 22:59:43 -0300

    Enabling OpenApi documentation

--- FilmesApi/Controllers/FilmesController.cs
+++ FilmesApi/Controllers/FilmesController.cs
@@ -21,7 +21,14 @@ namespace FilmesApi.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a inserção seja feita com sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
         {
             var filme = _mapper.Map<Filme>(filmeDto);   
--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -1,5 +1,7 @@
 using FilmesApi.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("FilmeConnectionString");
@@ -13,7 +15,13 @@ builder.Services
     .AddNewtonsoftJson(); //From NewtonSoft lib installed for AspNetCore.Mvc
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Filmes API", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
