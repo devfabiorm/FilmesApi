4e8aac59eb511fb5fd8c95c6303b7ca48bbea38c
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2024-02-22 14:17:02 -0300

    fix: Changed model to allow null values in reference column of movie theater

--- FilmesApi/Models/Sessao.cs
+++ FilmesApi/Models/Sessao.cs
@@ -10,8 +10,7 @@ namespace FilmesApi.Models
         [Required]
         public int FilmeId { get; set; }
         public virtual Filme Filme { get; set; }
-        [Required]
-        public int CinemaId { get; set; }
+        public int? CinemaId { get; set; }
         public virtual Cinema Cinema { get; set; }
     }
 }
